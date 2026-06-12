d15cd1736d7adf12b356e9127f160763c18a917b
Akira Takagi <tkgakrwork@gmail.com>
2026-06-12 19:36:25 +0900

    refactor: ヘルパー関数に置き換え

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -14,22 +14,23 @@ fn dies_no_args() -> TestResult {
     Ok(())
 }
 
-// 引数を与えて実行し、期待した結果で正常終了することを検証する
-#[test]
-fn hello1() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
-}
-
-#[test]
-fn hello2() -> TestResult {
-    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello", "there"])
+// 正常系テスト用のヘルパー関数
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
         .assert()
         .success()
         .stdout(expected);
     Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
 }
