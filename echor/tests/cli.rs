8e2f1a9bd6bfb64c2acc9b1a8923f75a22d3a975
Akira Takagi <tkgakrwork@gmail.com>
2026-06-12 19:12:38 +0900

    2.3.2 プログラムの出力を比較する

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 // 引数なしでプログラムを実行し、使い方が標準エラーに出力されることを検証する
 #[test]
@@ -10,9 +11,11 @@ fn dies_no_args() {
         .stderr(predicate::str::contains("USAGE"));
 }
 
-// 引数を与えて実行し、正常に終了することを検証する
+// 引数を与えて実行し、期待した結果で正常終了することを検証する
 #[test]
-fn runs() {
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
     let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
