315593d8c2bbaf8bcf6d90d40703970f65f7b4de
Akira Takagi <tkgakrwork@gmail.com>
2026-06-15 22:49:50 +0900

    修正

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -17,32 +17,32 @@ pub fn get_args() -> MyResult<Config> {
         .about("Rust cat")
         .arg(
             Arg::with_name("files")
-                .value_name("FILES")
+                .value_name("FILE")
                 .help("Input file(s)")
                 .multiple(true)
-                .min_values(1),
+                .default_value("-"),
         )
         .arg(
-            Arg::with_name("number_lines")
+            Arg::with_name("number")
                 .short("n")
-                .help("Number the output lines, starting at 1.")
-                .takes_value(false),
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
         )
         .arg(
-            Arg::with_name("number_nonblank_lines")
+            Arg::with_name("number_nonblank")
                 .short("b")
-                .help("Number the non-blank output lines, starting at 1.")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
                 .takes_value(false),
         )
         .get_matches();
 
-    let files = matches.values_of_lossy("files").unwrap();
-    let number_lines = matches.is_present("number_lines");
-    let number_nonblank_lines = matches.is_present("number_nonblank_lines");
     Ok(Config {
-        files,
-        number_lines,
-        number_nonblank_lines,
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
     })
 }
 
