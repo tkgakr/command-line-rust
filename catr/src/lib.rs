e883066d1fce812ff19fcad199594874dba8280c
Akira Takagi <tkgakrwork@gmail.com>
2026-06-15 22:42:11 +0900

    3.2.3 引数の定義

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,8 +1,52 @@
+use clap::{App, Arg};
 use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Akira Takagi<tkgakrwork@gmail.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file(s)")
+                .multiple(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .help("Number the output lines, starting at 1.")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("number_nonblank_lines")
+                .short("b")
+                .help("Number the non-blank output lines, starting at 1.")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let files = matches.values_of_lossy("files").unwrap();
+    let number_lines = matches.is_present("number_lines");
+    let number_nonblank_lines = matches.is_present("number_nonblank_lines");
+    Ok(Config {
+        files,
+        number_lines,
+        number_nonblank_lines,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
 }
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    if let Err(e) = catr::run() {
+    // get_args関数が Ok(config) を返した場合、and_then をつかって run に congif を渡す
+    if let Err(e) = catr::get_args().and_then(catr::run) {
+        //  get_args か run が Err を返したら標準エラーに出力
         eprintln!("{}", e);
         std::process::exit(1);
     }
