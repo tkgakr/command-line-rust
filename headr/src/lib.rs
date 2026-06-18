bbd5512dd3c13317adc33affad7613576c74f2e7
Akira Takagi <tkgakrwork@gmail.com>
2026-06-18 21:41:33 +0900

    4.2.1 文字列を数値に変換する機能の単体テスト

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -64,3 +64,28 @@ pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
     Ok(())
 }
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3は正の整数なのでOK
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // 文字列の場合はエラー
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // 0の場合はエラー
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
