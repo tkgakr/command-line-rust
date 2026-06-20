61916d7df66b941f912bfcfbb5c357cf47c69ed2
Akira Takagi <tkgakrwork@gmail.com>
2026-06-20 18:49:57 +0900

    4.3.1 ファイルを1行ずつ読み込む

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -68,7 +68,11 @@ pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(_) => println!("Opened {}", filename),
+            Ok(file) => {
+                for line in file.lines().take(config.lines) {
+                    println!("{}", line?);
+                }
+            }
         }
     }
     Ok(())
